7e11e43bea31e383e429c10b7f64c27b1d5e2589
qycl50224 <qy50224@163.com>
2020-05-30 23:56:12 +0800

    finished basic landing page

--- projects/microblog/app.js
+++ projects/microblog/app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes');
 
 var app = express();
 
@@ -19,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.get('/', routes.index);
 app.get("/u/:user", routes.user);
 app.post("/post", routes.post);
 app.get('/reg', routes.reg);
--- projects/microblog/routes/index.js
+++ projects/microblog/routes/index.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.index = function(req, res, next) {
+  res.render('index');
+};
+
+router.user = function(req, res) {
+};
+
+router.post = function(req, res) {
+};
+
+router.reg = function(req, res) {
+};
+
+router.doReg = function(req, res) {
+};
+
+router.login = function(req, res) {
+};
+
+router.doLogin = function(req, res) {
+};
+
+router.logout = function(req, res) {
+};
 
 module.exports = router;
