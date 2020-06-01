d126d609c09a477e974b5d42ed38926bce50a83e
qycl50224 <qy50224@163.com>
2020-06-01 16:40:04 +0800

    add local variable user

--- projects/microblog/app.js
+++ projects/microblog/app.js
@@ -7,9 +7,14 @@ var methodOverride = require('method-override');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-var MongoStore = require('connect-mongo')(session);
 var setting = require('./setting');
-var session = require('express-session');
+var session = require('express-session'); // 注意这个放在下面这个之上
+var MongoStore = require('connect-mongo')(session);
+
+var middleware = require('./middleware');
+
+
+
 
 var User = require('./models/user');
 
@@ -24,36 +29,45 @@ var port = process.env.PORT || 3000;
 mongoose.connect("mongodb://localhost/microblog");
 
 // view engine setup
-app.configure(function(){
-	app.set('views', path.join(__dirname, 'views'));
-	app.set('view engine', 'ejs');
-	app.use(bodyParser.json());
-	app.use(methodOverride("_method"));
-	app.use(bodyParser.urlencoded({ extended: true }));
-	app.use(cookieParser());
-	app.use(express.static(path.join(__dirname, 'public')));
-	app.use(session({
-		secret: setting.cookie_secret,
-		store: new MongoStore({
-			db: setting.db
-		})
-	}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(flash());
+app.use(bodyParser.json());
+app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser('secret'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+	secret: setting.cookie_secret,
+	store: new MongoStore({
+		url:'mongodb://localhost/'+setting.db,
+		autoRemove:'native'
+	})
+}));
 
-	app.use(logger('dev'));
-	app.use(express.json());
+app.use(logger('dev'));
+app.use(express.json());
+
+
+// 给所有页面，用于上面的导航栏，添加一个本地变量
+app.use(function(req, res, next){
+	res.locals.user = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
 });
 
 app.get('/', routes.index);
 app.get("/u/:user", routes.user);
-app.post("/post", routes.post);
-app.get('/reg', routes.reg);
-app.post('/reg', routes.doReg);
+app.post("/post", middleware.checkLogin, routes.post);
+app.get('/reg', middleware.checkNotLogin, routes.reg);
+app.post('/reg', middleware.checkNotLogin, routes.doReg);
 
 
 
-app.get('/login', routes.login);
-app.post('/login', routes.doLogin);
-app.get('/logout', routes.logout);
+app.get('/login', middleware.checkNotLogin, routes.login);
+app.post('/login', middleware.checkNotLogin, routes.doLogin);
+app.get('/logout', middleware.checkLogin, routes.logout);
 
 
 // 视图交互
@@ -65,6 +79,7 @@ app.use(function(req, res, next) {
 	res.locals.success = success.length? success:null;
 	next();
 });
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
