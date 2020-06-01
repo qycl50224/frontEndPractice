8d3a941d06f057e8a80326bb0db232fd7490d353
qycl50224 <qy50224@163.com>
2020-06-01 12:34:47 +0800

    add package

--- projects/microblog/routes/index.js
+++ projects/microblog/routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
 /* GET home page. */
@@ -13,12 +14,33 @@ router.post = function(req, res) {
 };
 
 router.reg = function(req, res) {
+	res.render('reg');
 };
 
 router.doReg = function(req, res) {
+	var newUser = new User({username: req.body.username});
+	if(req.body.code === 'sduwh'){
+		User.register(newUser, req.body.password, function(err, user){
+			if(err){
+				console.log(err);
+				req.flash("error", err.message);
+				return res.render("reg");
+			} else {
+				passport.authenticate("local")(req, res, function(){
+					req.flash("success",  user.username + " 已经上线");
+					res.redirect("/index");
+				});	
+			}
+		});
+	} else {
+		req.flash("error: false code");
+		res.redirect("/reg");
+	}
 };
 
+
 router.login = function(req, res) {
+	res.render('login');
 };
 
 router.doLogin = function(req, res) {
---
+++ projects/microblog/setting.js
@@ -0,0 +1,5 @@
+module.exports={
+	cookieSecret:'microblogbyevin',
+	db:'microblog',
+	host:'localhost'
+}
