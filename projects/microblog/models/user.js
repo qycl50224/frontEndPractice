db597ff522ab401b6b2245aa5f21749479cd7959
qycl50224 <qy50224@163.com>
2020-06-01 13:18:26 +0800

    add function User.protorype.save

--- projects/microblog/models/user.js
+++ projects/microblog/models/user.js
@@ -1,13 +1,57 @@
-var mongoose = require("mongoose");
-var passportLocalMongoose = require("passport-local-mongoose");
+var mongodb = require('./db');
 
-var UserSchema = new mongoose.Schema({
-	username: String,
-	password: String,
-	isAdmin: {type: Boolean, default: false}
-});
 
-UserSchema.plugin(passportLocalMongoose);
+function User(user) {
+	this.username = user.username;
+	this.password = user.password;
+}
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = User;
 
+User.prototype.save = function save(callback) {
+	var user = {
+		username: this.username;
+		password: this.password
+	};
+	mongodb.open(function(err, db) {
+		if (err) {
+			return callback(err);
+		};
+		// 读取user集和
+		db.collection('users', function(err, collection) {
+			if (err) {
+				mongodb.close;
+				return callback(err);
+			}
+			// 为name属性添加索引
+			collection.ensureIndex('username', {unique:true});
+			// 写入user文档
+			collection.insert(user, {safe:true}, function(err, user) {
+				mongodb.close();
+				callback(err, user);
+			});
+		});
+	});
+};
+
+User.get = function get(username, callback) {
+	mongodb.open(function(err, db) {
+		if (err) {
+			return callback(err);
+		}
+		db.collection('users', function(err, collection) {
+			if (err) {
+				mongodb.close();
+				return callback(err);
+			}
+			collection.findOne({name:username}, function(err, doc) {
+				mongodb.close();
+				if (doc) {
+					var user = new User(doc);
+				} else {
+					callback(err, null);
+				}
+			})
+		})
+	})
+}
